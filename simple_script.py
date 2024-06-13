ee11951c0e93e2dd3c8b1d4101d3ad49f8d72268
miroslav.velich <miroslav.velich@ausy.com>
2024-06-13 11:30:05 +0300

    simple add 3 random ints

---
+++ simple_script.py
@@ -0,0 +1,14 @@
+import random
+
+rand1 = random.randint(0, 10)
+rand2 = random.randint(0, 10)
+rand3 = random.randint(0, 10)
+
+def add_vars():
+    result = rand1+rand2+rand3
+    print("rand1 is:",rand1)
+    print("rand2 is:", rand2)
+    print("rand3 is:", rand3)
+    print("Result of rand1/2/3 is:",result)
+
+add_vars()
